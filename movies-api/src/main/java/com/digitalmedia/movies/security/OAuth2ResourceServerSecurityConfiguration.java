0d9c035fb73abfa304efcf7f3c570f4a8542a4b4
mceciabate <m.cecilia.abate@gmail.com>
2023-03-13 14:15:26 -0300

    movies

--- movies-api/src/main/java/com/digitalmedia/movies/security/OAuth2ResourceServerSecurityConfiguration.java
+++ movies-api/src/main/java/com/digitalmedia/movies/security/OAuth2ResourceServerSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
@@ -15,7 +16,8 @@ public class OAuth2ResourceServerSecurityConfiguration extends WebSecurityConfig
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(new KeyCloakJwtAuthenticationConverter());
-    http.authorizeRequests().anyRequest().authenticated();
+    http.cors().and().csrf().disable()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests().anyRequest().authenticated();
     //http.authorizeRequests().antMatchers("/actuator/**").permitAll();
   }
   @Bean
