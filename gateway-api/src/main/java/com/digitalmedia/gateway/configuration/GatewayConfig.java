0c88701d8d2cba44d0565c61f2ea6d4888d60405
mceciabate <m.cecilia.abate@gmail.com>
2023-03-13 23:05:38 -0300

    gateway

--- gateway-api/src/main/java/com/digitalmedia/gateway/configuration/GatewayConfig.java
+++ gateway-api/src/main/java/com/digitalmedia/gateway/configuration/GatewayConfig.java
@@ -5,7 +5,9 @@ import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterF
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class GatewayConfig {
     @Autowired
     private TokenRelayGatewayFilterFactory filterFactory;
@@ -18,10 +20,3 @@ public class GatewayConfig {
                 .build();
     }
 }
-/*	@Bean
-	public RouteLocator routes(RouteLocatorBuilder builder) {
-		return builder.routes().route("auth", r -> r.path("/auth/**").filters(f -> f.filter(filter)).uri("lb://auth"))
-				.route("alert", r -> r.path("/alert/**").filters(f -> f.filter(filter)).uri("lb://alert"))
-				.route("echo", r -> r.path("/echo/**").filters(f -> f.filter(filter)).uri("lb://echo"))
-				.route("hello", r -> r.path("/hello/**").filters(f -> f.filter(filter)).uri("lb://hello")).build();
-	}*/
--- movies-api/src/main/java/com/digitalmedia/movies/security/OAuth2ResourceServerSecurityConfiguration.java
+++ movies-api/src/main/java/com/digitalmedia/movies/security/OAuth2ResourceServerSecurityConfiguration.java
@@ -1,27 +1,35 @@
 package com.digitalmedia.movies.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
+@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class OAuth2ResourceServerSecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-  @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(new KeyCloakJwtAuthenticationConverter());
-    http.cors().and().csrf().disable()
-//            http.oauth2ResourceServer().jwt().jwkSetUri()
-                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                    .and().authorizeRequests().anyRequest().authenticated();
-    //http.authorizeRequests().antMatchers("/actuator/**").permitAll();
+    http.authorizeRequests(authorize -> authorize.anyRequest().authenticated())
+            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
   }
+
+
+//  @Override
+//  protected void configure(HttpSecurity http) throws Exception {
+//    http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(new KeyCloakJwtAuthenticationConverter());
+//    http.cors().and().csrf().disable()
+//
+//                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                    .and().authorizeRequests().anyRequest().authenticated();
+//}
+
   @Bean
   public JwtDecoder jwtDecoder() {
     return NimbusJwtDecoder.withJwkSetUri("http://localhost:8085/realms/DH/protocol/openid-connect/certs").build();
